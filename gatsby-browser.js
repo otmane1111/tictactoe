8ac4129cbebf44d05cac930946dd2de0fd64b505
otmane1111 <152818717+otmane1111@users.noreply.github.com>
2023-12-16 15:38:24 +0100

    Add files via upload

---
+++ gatsby-browser.js
@@ -0,0 +1,15 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
+ */
+
+// You can delete this file if you're not using it
+// gatsby-browser.js ou gatsby-ssr.js
+
+// import React from "react";
+// import { FirebaseProvider } from "./src/firebase";
+
+// export const wrapRootElement = ({ element }) => {
+//   return <FirebaseProvider>{element}</FirebaseProvider>;
+// };
